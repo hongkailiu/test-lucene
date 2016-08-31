61780b09ccea7b4547474893ba191e266ac7152a
hongkailiu <hongkailiu@users.noreply.github.com>
2016-08-30 22:53:53 -0400

    another hello world

---
+++ src/test/java/tk/hongkailiu/test/lucene/HelloLucene1Test.java
@@ -0,0 +1,18 @@
+package tk.hongkailiu.test.lucene;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by hongkailiu on 2016-08-30.
+ */
+public class HelloLucene1Test {
+
+    private HelloLucene1 hello = new HelloLucene1();
+    @Test
+    public void start() throws Exception {
+        hello.start();
+    }
+
+}
